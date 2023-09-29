64cb43e1525ccb25f207d71f130fb00684d4bfd3
Gui <gsetienne@free.fr>
2023-09-29 09:49:58 +0200

    excursions toggle et avancement de la page

--- js/burger.js
+++ js/burger.js
@@ -20,17 +20,27 @@ function closeNav() {
   sidenav.classList.remove("active");
 }
 
-// année automatique
+// --------------------- année automatique
 const date = new Date();
 const year = date.getFullYear();
 document.getElementById("activeyear").innerHTML = year;
 
 
-// Toggle
-const toggles = document.querySelectorAll('.toggle');
-  toggles.forEach((toggle) => {
-    const target = document.querySelector(toggle.getAttribute('data-target'));
-    toggle.addEventListener('click', () => {
-      target.classList.toggle('visible');
+// ---------------------- Toggle
+// Récupérez tous les boutons et le texte supplémentaire
+const toggleButtons = document.querySelectorAll('.toggle-button');
+const additionalTexts = document.querySelectorAll('.additional-text');
+
+// Ajoutez un gestionnaire d'événements de clic à chaque bouton
+toggleButtons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        // Affichez ou masquez le texte supplémentaire en fonction de son état actuel
+        if (additionalTexts[index].style.display === 'none' || additionalTexts[index].style.display === '') {
+            additionalTexts[index].style.display = 'block';
+            button.textContent = ' < ';
+        } else {
+            additionalTexts[index].style.display = 'none';
+            button.textContent = ' > ';
+        }
     });
-  });
+});
